8fcb853d8ef47606576af7b77c0cdb28c6f730e3
GITHUBSARIKA <league@WTS22.attlocal.net>
2018-08-16 18:29:54 -0700

    f

--- src/_02_Firework_Display/Firework.java
+++ src/_02_Firework_Display/Firework.java
@@ -71,7 +71,10 @@ public class Firework {
 
 	public void drawSparks(Graphics g) {
 		// 5. Iterate through each spark
-
+		for (int i = 0; i < sparks.length; i++) {
+			g.setColor(sparks[i].color);
+			g.fillOval(sparks[i].x, sparks[i].y, sparks[i].size, sparks[i].size);
+		}
 		// 6. Set the color to the spark's color with g.setColor()
 		// 7. Fill in an oval using the spark's x, y, and size with g.fillOval()
 	}
